42ca1ee73055f8cb94bc04c01f822fcc9f0013ba
youxiaobin <youxiaobin@jd.com>
2022-11-02 21:30:35 +0800

    完善注释

--- Board_Linux/led01/leds.s
+++ Board_Linux/led01/leds.s
@@ -1,28 +1,32 @@
 .global _start /* start entry */
 
 _start:
-    // enable CCM_CCGR1 for GPIO01
+    // 找到控制GPIO1_IO03寄存器的使能时钟
+    // 开启使能时钟
     ldr r0, =0x020C406C //set r0 CCM_CCGR1(GPIO1) address
     ldr r1, =0xFFFFFFFF //set r1 value
     str r1, [r0]        //r1 value to r0 address CCGR resiter, enable GPIO1 enable(26-27 bits)
 
     // mux GPIO1_IO03
-
+    // SW_MUX_CTL_PAD_GPIO1_IO03
     ldr r0, =0x020E0068
     ldr r1, =0x5        //0101
     str r1, [r0]
 
     // 配置电气属性
-    
+    // SW_PAD_CTL_PAD_GPIO1_IO03
     ldr r0, =0x020E02F4
     /**
-        bit0:           0       设置速率为低速率
-        bit5:3          110     设置驱动能力为R0/6，比较强的驱动能力
+        电器参数位详解: https://z.itpub.net/article/detail/578088950F3DA3F701FB5454715771BB     
+        上拉/下拉电阻: https://www.bilibili.com/video/BV1nf4y1y7Xx/?spm_id_from=333.337.search-card.all.click&vd_source=89f42a8ea34957e3baa89069cfc1bb28
+                      https://www.bilibili.com/video/BV1W34y1579U/?spm_id_from=333.788.recommend_more_video.-1&vd_source=89f42a8ea34957e3baa89069cfc1bb28
+        bit0:           0       设置压摆率为低压摆率
+        bit5:3          110     设置驱动能力为R0/6，比较强的驱动能力(驱动能力则是输出电流的能力,驱动能力不足或者负载电流过大导致驱动能力不足时可能导致逻辑电路电压不稳定或者不足,从而导致逻辑电路紊乱)
         bit7:6          10      设置速度为中速度
-        bit11           0       关闭开路输出
-        bit12           1       全能pull/keeper
-        bit13           0       保持上拉
-        bit15:14        00      100k下拉
+        bit11           0       关闭开漏输出(使用推挽输出： 驱动能力强)
+        bit12           1       使能（开启）上下拉/保持器功能 保持器: 但是它在输入模式下，叫输入状态保持器，用于当外部电路断电时（此时 MCU 还是供电的），维持 IO 口的状态，有利于低功耗应用。而在输出模式下，它又叫输出状态保持器，用于当内核断电时，维持 IO 口的输出状态，同样有利于低功耗应用。
+        bit13           0       设置为使用保持器
+        bit15:14        00      100k下拉(将电平钳位在低电平，过滤掉其它可能的干扰电平导致的信号紊乱，因为此时的LED灯需要低电平导通)
         bit16           0       关闭hys
      */
      ldr r1, =0x10B0
