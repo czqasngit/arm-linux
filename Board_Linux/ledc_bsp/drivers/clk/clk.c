09315de580ef7380b8e5950db08fa511cac9ff3d
yxb <305107316@qq.com>
2022-11-18 23:38:07 +0800

    完成beep

--- Board_Linux/ledc_bsp/drivers/clk/clk.c
+++ Board_Linux/ledc_bsp/drivers/clk/clk.c
@@ -3,5 +3,11 @@
 
 void Clk_Init()
 {
+    CCM->CCGR0 = 0xFFFFFFFF;
     CCM->CCGR1 = 0xFFFFFFFF;
+    CCM->CCGR2 = 0xFFFFFFFF;
+    CCM->CCGR3 = 0xFFFFFFFF;
+    CCM->CCGR4 = 0xFFFFFFFF;
+    CCM->CCGR5 = 0xFFFFFFFF;
+    CCM->CCGR6 = 0xFFFFFFFF;
 }
--- Board_Linux/ledc_bsp/drivers/clk/clk.h
+++ Board_Linux/ledc_bsp/drivers/clk/clk.h
@@ -2,5 +2,5 @@
 #define CONST_CLK_H
 #endif
 
-/// @brief 初始化(使能)外设引用
+/// @brief 初始化(使能)所有I.MX6ULL外设引脚
 void Clk_Init();
