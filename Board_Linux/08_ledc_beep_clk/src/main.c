ba2d2b11266165d449f67a681a1380d71e6bef3f
youxiaobin <youxiaobin@jd.com>
2022-12-13 09:17:01 +0800

    add clk init driver example

---
+++ Board_Linux/08_ledc_beep_clk/src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "delay.h"
+#include "clk.h"
+#include "led.h"
+#include "beep.h"
+
+int main(void)
+{
+    Clk_Init();
+    Led_Init();
+    Beep_Init();
+    while (1)
+    {
+        Delay(500);
+        Led_On();
+        Beep_On();
+        Delay(500);
+        Led_Off();
+        Beep_Off();
+    }
+
+    return 0;
+}
