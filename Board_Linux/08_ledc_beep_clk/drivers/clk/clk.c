d97e5a90a0fb80f4995664a3d0ac004b5f1308b5
youxiaobin <youxiaobin@jd.com>
2022-12-19 15:31:11 +0800

    完成

--- Board_Linux/08_ledc_beep_clk/drivers/clk/clk.c
+++ Board_Linux/08_ledc_beep_clk/drivers/clk/clk.c
@@ -15,18 +15,17 @@ void Clk_Init()
 void Clk_Freq_Init()
 {
     /// CCM->CCSR的第二位判断是否选择的是pll1_main_clk(值是0)
-    if ((CCM->CCSR >> 2 & 0x1) == 0)
+    if (((CCM->CCSR >> 2) & 0x1) == 0)
     {
         /// 需要先切换时钟源到24MHz晶振
         CCM->CCSR &= ~(1 << 8);
         CCM->CCSR |= (1 << 2);
     }
     /// bit 13: enable register
-    CCM_ANALOG->PLL_ARM |= (1 << 13);
-    /// 配置
-    CCM_ANALOG->PLL_ARM |= 58;
-    /// 配置一分频
-    CCM->CACRR = 0;
+    /// 配置(696MHz)
+    CCM_ANALOG->PLL_ARM = (1 << 13) | (58);
     /// 将时钟源切换回CCSR的第二位判断是否选择的是pll1_main_clk
     CCM->CCSR &= ~(1 << 2);
+    /// 分频器配置为1
+    CCM->CACRR = 0;
 }
