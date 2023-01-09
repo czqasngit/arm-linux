6558bdbbb3b57f1a30b1c2c509d6a89e8bf60348
youxiaobin <youxiaobin@jd.com>
2023-01-09 20:50:47 +0800

    rtc

--- Board_Linux/17_i2c/drivers/i2c/i2c.c
+++ Board_Linux/17_i2c/drivers/i2c/i2c.c
@@ -1,1 +1,24 @@
 #include "i2c.h"
+
+void I2C_Init(I2C_Type *i2c) {
+    i2c->I2CR &= ~(1 << 7); // 关闭i2c
+    i2c->IFDR = 0x15;       // 设置i2c的分频值是640, 66000000 / 640 = 103.125 KHz; i2c支持的频率范围是(100kbit/s - 400kbit/s)
+    i2c->I2CR |= (1 << 7);  // 使能i2c
+}
+
+char I2C_Master_Start(I2C_Type *i2c, unsigned int slave_addr, I2CDirection direction) {
+    if((i2c->I2SR >> 5) & 0x1 == 1) {
+        return 1;
+    }
+    // 设置为主机模式
+    i2c->I2CR |= (1 << 5);
+    // 设置为发送模式(在寻址时)
+    i2c->I2CR |= (1 << 4);
+    // 设置I2DR寻址,同步产生一个start信号
+    // 高7位是地址,0位是读写位
+    i2c->I2DR = (unsigned int)((slave_addr << 1) | direction);
+}
+
+char I2C_Master_Stop(I2C_Type *i2c) {
+    
+}
--- Board_Linux/17_i2c/drivers/i2c/i2c.h
+++ Board_Linux/17_i2c/drivers/i2c/i2c.h
@@ -3,18 +3,28 @@
 
 
 enum I2CStatus {
-    OK = 0, 
-    BUSY = 1, 
-    IDLE = 2, 
-    NAK = 3, 
-    ARBI = 4/*仲裁丢失*/, 
-    TIMEOUT = 5
+    I2CStatus_OK = 0, 
+    I2CStatus_BUSY = 1, 
+    I2CStatus_IDLE = 2, 
+    I2CStatus_NAK = 3, 
+    I2CStatus_ARBI = 4,           /* 仲裁丢失 */
+    I2CStatus_TIMEOUT = 5,        /* 超时 */
+    I2CStatus_ACK = 7             
 }
 enum I2CDirection {
-    WRITE = 0,
-    READ = 1
+    I2CDirection_WRITE = 0,
+    I2CDirection_READ = 1
 }
 #include "imx6ul.h"
 #include "MCIMX6Y2.h"
 
+/// @brief 初始化I2C
+/// @param i2c I2C设备
+void I2C_Init(I2C_Type *i2c);
+/// @brief 作为主机产生一个起始信号,寻址从机
+/// @param i2c I2C设备
+/// @param slave_addr 从机地址
+/// @param direction  读写方向
+/// @result 0: I2C忙, 1: I2C空闲
+char I2C_Master_Start(I2C_Type *i2c, unsigned int slave_addr, I2CDirection direction);
 #endif
