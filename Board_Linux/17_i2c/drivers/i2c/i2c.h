a3b3afcc445054e87169af11707be799b8c2b963
youxiaobin <youxiaobin@jd.com>
2023-01-09 19:57:15 +0800

    rtc

--- Board_Linux/17_i2c/drivers/i2c/i2c.h
+++ Board_Linux/17_i2c/drivers/i2c/i2c.h
@@ -1,6 +1,19 @@
 #ifndef _I2C_H
 #define _I2C_H
 
+
+enum I2CStatus {
+    OK = 0, 
+    BUSY = 1, 
+    IDLE = 2, 
+    NAK = 3, 
+    ARBI = 4/*仲裁丢失*/, 
+    TIMEOUT = 5
+}
+enum I2CDirection {
+    WRITE = 0,
+    READ = 1
+}
 #include "imx6ul.h"
 #include "MCIMX6Y2.h"
 
