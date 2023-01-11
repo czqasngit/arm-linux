f08e045402f37908c43102acbdef41feb06ea53c
youxiaobin <youxiaobin@jd.com>
2023-01-11 16:44:54 +0800

    ap3216c

--- Board_Linux/18_i2c/drivers/ap3216c/ap3216c.c
+++ Board_Linux/18_i2c/drivers/ap3216c/ap3216c.c
@@ -1,1 +1,16 @@
 #include "ap3216c.h"
+#include "imx6ul.h"
+#include "MCIMX6Y2.h"
+#include "i2c.h"
+
+void AP3216C_Init() {
+    // 引脚复用
+    IOMUXC_SetPinMux(IOMUXC_UART4_TX_DATA_I2C1_SCL, 1)
+    IOMUXC_SetPinConfig(IOMUXC_UART4_TX_DATA_I2C1_SCL, 0x70b0);
+
+    IOMUXC_SetPinMux(IOMUXC_UART4_RX_DATA_I2C1_SDA, 1);
+    IOMUXC_SetPinConfig(IOMUXC_UART4_RX_DATA_I2C1_SDA, 0X70b0);
+
+    // 初始化I2C主机
+    I2C_Init(I2C1);
+}
--- Board_Linux/18_i2c/drivers/ap3216c/ap3216c.h
+++ Board_Linux/18_i2c/drivers/ap3216c/ap3216c.h
@@ -1,8 +1,19 @@
 #ifndef _AP3216C_H_
 #define _AP3216C_H_
 
-#include "i2c.h"
-#include "imx6ul.h"
-#include "MCIMX6Y2.h"
+#define AP3216C_ADDR                            0x1E        // I2C Slave Address
+
+#define AP3216C_SYSTEM_CONFIGURATION_REGISTER   0x00
+#define AP3216C_INPUT_STATUS                    0x01
+#define AP3216C_INT_MANAGER                     0x02
+#define AP3216C_IR_DATA_LOW                     0X0A
+#define AP3216C_IR_DATA_HIGH                    0X0B
+#define AP3216C_ALS_DATA_HIGH                   0X0C
+#define AP3216C_ALS_DATA_HIGH                   0X0D
+#define AP3216C_PS_DATA_HIGH                    0X0E
+#define AP3216C_PS_DATA_HIGH                    0X0F
+
+
+void AP3216C_Init();
 
 #endif
